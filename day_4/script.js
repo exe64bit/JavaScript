44a89dece3227c636bc18204540e4184925889a2
Sk Saidul Islam <177244130+exe64bit@users.noreply.github.com.>
2026-08-22 12:14:27 +0530

    day5 commit

--- day_4/script.js
+++ day_4/script.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+function aaa() {
+    var a = 1
+    var b = 2
+
+    var c = a+b
+    return 10
+}
--- day_5/script.js
+++ day_5/script.js
@@ -1,1 +1,10 @@
-console.log("Day 5");
+function hero() {
+    console.log("I am hero function");
+}
+function main(a) {
+    console.log("This is main function.");
+    
+    console.log(a());
+    a()
+}
+main(hero)
