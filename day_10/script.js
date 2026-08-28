de44d9d4b5d719d5c316ee56363244ee2a2cfd12
Sk Saidul Islam <177244130+exe64bit@users.noreply.github.com.>
2026-08-28 18:39:22 +0530

    day_10

--- day_10/script.js
+++ day_10/script.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+function CreateBook(bookName, bookAuther, bookPages){  
+    this.bookName = bookName
+    this.bookAuther = bookAuther
+    this.bookPages = bookPages
+}
+
+let book1 = new CreateBook("Physics", "HC Verma", 340)
+
+console.log(book1);
+
+
+
--- day_9/script.js
+++ day_9/script.js
@@ -2,4 +2,4 @@ let arr = [10, 20, 30, 40]
 
 arr.name = 50
 
-console.log(arr);0
+console.log(arr);
